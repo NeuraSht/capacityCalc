0847ecde00967495659a92b424075f0b9a43f8f5
Denis Shteinbakh <mmddendi@gmail.com>
2024-05-22 20:38:33 +0200

    add font

--- capacityCalc.py
+++ capacityCalc.py
@@ -3,6 +3,12 @@ import dearpygui.dearpygui as dpg
 dpg.create_context()
 dpg.create_viewport(title='Custom Title', width=700, height=300)
 
+with dpg.font_registry():
+    with dpg.font(f'C:\\Windows\\Fonts\\Calibri.ttf', 18) as default_font:
+        dpg.add_font_range_hint(dpg.mvFontRangeHint_Default)
+        dpg.add_font_range_hint(dpg.mvFontRangeHint_Cyrillic)
+        dpg.bind_font(default_font)
+
 with dpg.window(label="Example Window"):
     dpg.add_text("Hello, world")
     dpg.add_button(label="Save")
